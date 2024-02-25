25b4c79caaa0860d689130848cc6f6821d22ee84
AleksandrVishniakov <vishniakov.alexandr@gmail.com>
2024-02-25 20:39:13 +0300

    feat: replace server starting to run() function, add os.Interrupt and context cancel listening to shut down http server

--- orders-service/app/cmd/app/main.go
+++ orders-service/app/cmd/app/main.go
@@ -4,23 +4,56 @@ import (
 	"context"
 	"github.com/AleksnadrVishniakov/versta-2024/orders-service/app/internal/handlers"
 	"github.com/AleksnadrVishniakov/versta-2024/orders-service/app/internal/servers"
+	"github.com/AleksnadrVishniakov/versta-2024/orders-service/app/pkg/logger"
 	"io"
 	"log"
 	"log/slog"
 	"os"
-
-	"github.com/AleksnadrVishniakov/versta-2024/orders-service/app/pkg/logger"
+	"os/signal"
 )
 
 const logFileName = "app.log"
 
-var (
-	httpPort = os.Getenv("HTTP_PORT")
-	logLevel = os.Getenv("LOG_LEVEL")
-)
+func run(
+	ctx context.Context,
+	getenv func(string) string,
+	writer io.Writer,
+) error {
+	var (
+		httpPort = getenv("HTTP_PORT")
+		logLevel = getenv("LOG_LEVEL")
+	)
+
+	logger.InitLogger(writer, logLevel)
+
+	handler := handlers.NewHTTPHandler()
+
+	server := servers.NewHTTPServer(httpPort, handler.InitRoutes())
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				slog.Info("server is shutting down")
+
+				if err := server.Shutdown(ctx); err != nil {
+					slog.Error("error shutting down http server", slog.String("error", err.Error()))
+				}
+			}
+
+		}
+	}()
+
+	slog.Info("server started on http://localhost:" + httpPort)
+	if err := server.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
-	var ctx, cancel = context.WithCancel(context.Background())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
 	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
@@ -29,18 +62,10 @@ func main() {
 	}
 	defer logFile.Close()
 
-	logger.InitLogger(io.MultiWriter(os.Stdout, logFile), logLevel)
+	logWriter := io.MultiWriter(os.Stdout, logFile)
 
-	handler := handlers.NewHTTPHandler()
-
-	server := servers.NewHTTPServer(httpPort, handler.InitRoutes())
-
-	slog.Info("server started on http://localhost:" + httpPort)
-
-	if err := server.Run(); err != nil {
-		err := server.Shutdown(ctx)
-		if err != nil {
-			log.Println("server shutdown error:", err.Error())
-		}
+	if err := run(ctx, os.Getenv, logWriter); err != nil {
+		slog.Error("server running error", slog.String("error", err.Error()))
+		os.Exit(1)
 	}
 }
